18a868bef2302120d1c56159559a83dfedb9ab85
arunkp2k18 <34996270+arunkp2k18@users.noreply.github.com>
2020-06-02 15:17:46 +0530

    Create bayt_spider.py

---
+++ bayt_spider.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import BaytTutorialItem
+
+class BaytSpiderSpider(scrapy.Spider):
+    name = 'bayt_spider'
+    start_urls = ['https://www.bayt.com/en/uae/jobs/q/accountant/']
+
+    def parse(self, response):
+        items = BaytTutorialItem()
+        for jobs in response.css('li.has-pointer-d'):
+            job = jobs.css('.t-regular a::text').extract_first()
+            items['job_name'] = job.strip()
+            company = jobs.css('.p10r::text').extract_first()
+            items['company'] = company.strip()
+            date = jobs.css('div.t-small::text').extract()[1]
+            items['Date'] = date.strip()
+            yield items
+
+        next=response.css('li.pagination-next a::attr(href)').get()
+        if next is not None:
+            next_page = next.split('/')[-1]
+            yield response.follow(next_page, callback= self.parse)
+
+
+
+        #for jobs in response.css('.t-regular a::text').extract():
+            #job_name = jobs.strip()
+            #items['job_name'] = job_name
+            #items['company'] = response.css('.p10r::text').extract()
+            #yield items
+        #job_name = response.css('.t-regular a::text').extract_first().strip()
+        #for companies in response.css('.p10r::text').extract():
+        #    items['company'] = companies.strip().replace('\t','')
+        #    yield items
